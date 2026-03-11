33e3f7044ba72493c8414dfd3dd7b29bb9669a6e
adwaith85 <adwaithadhu85227@gmail.com>
2026-03-11 12:10:57 +0530

    icon changed

--- frontend/src/Axios/Script.js
+++ frontend/src/Axios/Script.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://shop-cart-api.adwaithh.online",
-    baseURL: "http://localhost:8000",
+    baseURL: "https://shop-cart-api.adwaithh.online",
+    // baseURL: "http://localhost:8000",
     withCredentials: true,
 });
 
